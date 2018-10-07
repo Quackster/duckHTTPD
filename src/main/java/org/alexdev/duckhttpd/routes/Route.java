0a7565c9b6bf2d295313984857d5fc82299f0fe7
Alex <quacksterr.15@gmail.com>
2018-10-07 11:00:18 +1000

    Remove construct requirement

--- src/main/java/org/alexdev/duckhttpd/routes/Route.java
+++ src/main/java/org/alexdev/duckhttpd/routes/Route.java
@@ -3,6 +3,5 @@ package org.alexdev.duckhttpd.routes;
 import org.alexdev.duckhttpd.server.connection.WebConnection;
 
 public interface Route {
-    void construct(WebConnection client);
     void handleRoute(WebConnection client) throws Exception;
 }
