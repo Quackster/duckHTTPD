79f014fd3ab2b186a2269cb8b43d9fc6674e925e
Alex <quacksterr.15@gmail.com>
2018-10-23 14:42:08 +1000

    Apply no cache to custom responses

--- src/main/java/org/alexdev/duckhttpd/response/ResponseBuilder.java
+++ src/main/java/org/alexdev/duckhttpd/response/ResponseBuilder.java
@@ -47,6 +47,7 @@ public class ResponseBuilder {
 
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
         response.headers().set(HttpHeaderNames.CONTENT_LENGTH, data.length);
+        applyNoCache(response);
         return response;
     }
 
@@ -59,7 +60,15 @@ public class ResponseBuilder {
 
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
         response.headers().set(HttpHeaderNames.CONTENT_LENGTH, data.length);
+        applyNoCache(response);
         return response;
+    }
+
+
+    private static void applyNoCache(FullHttpResponse response) {
+        response.headers().add(HttpHeaderNames.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        response.headers().add(HttpHeaderNames.PRAGMA, "no-cache");
+        response.headers().add(HttpHeaderNames.EXPIRES, "0");
     }
 
     public static boolean create(File file, WebConnection conn) throws Exception {
