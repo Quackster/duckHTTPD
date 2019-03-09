8477cef19229221cab3e9c0554c98af012b6b5b3
Alex <quacksterr.15@gmail.com>
2019-03-09 11:30:24 +1000

    Use UTF-8

--- src/main/java/org/alexdev/duckhttpd/server/WebChannelInitializer.java
+++ src/main/java/org/alexdev/duckhttpd/server/WebChannelInitializer.java
@@ -5,14 +5,18 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.flow.FlowControlHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.CharsetUtil;
 
 public class WebChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     public void initChannel(final SocketChannel ch) throws Exception {
+
         ch.pipeline().addLast(new HttpServerCodec());
+        ch.pipeline().addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8));
         ch.pipeline().addLast(new HttpObjectAggregator(512 * 1024));
         ch.pipeline().addLast(new ChunkedWriteHandler());
         ch.pipeline().addLast(new WebChannelHandler());
