2ca5dc5127381da27f7153ee9990e0cfb7fcf997
Quackster <quacksterr.15@gmail.com>
2021-10-23 15:55:12 +1000

    Update Main.java

--- src/main/java/org/alexdev/duckhttpd/Main.java
+++ src/main/java/org/alexdev/duckhttpd/Main.java
@@ -8,56 +8,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        /*String test = "(.*?)://(.*?)test";
-        String toReplace = "https://$2test";
-
-        Pattern pattern = Pattern.compile(test);
-
-        // get a matcher object from pattern
-        Matcher matcher = pattern.matcher("https://bigtest");
-
-        List<String> list = new ArrayList<>();
-
-        if (matcher.matches()) {
-            for (int i = 1; i < matcher.groupCount() + 1; i++) {
-                list.add(matcher.group(i));
-            }
-        }
-
-        int i = 1;
-        for (var entry : list) {
-            toReplace = toReplace.replace("$" + i, entry);
-            i++;
-        }
-
-        System.out.println(toReplace);*/
-
-        /*PageRules.getInstance().addBlacklist("*://classichabbo.com/block*");
-        PageRules.getInstance().addRule("*://classichabbo.com/*", "https://classichabbo.com/$2");
-
-        System.out.println("test 1");
-        String url = "http://classichabbo.com/test";
-
-        var matches = PageRules.getInstance().matchesRule(url);
-
-        if (matches != null) {
-            System.out.println(PageRules.getInstance().getNewUrl(matches, url));
-        }
-
-        System.out.println("test 2");
-        url = "://classichabbo.com/block/432432";
-        matches = PageRules.getInstance().matchesRule(url);
-
-        if (matches != null) {
-            System.out.println(PageRules.getInstance().getNewUrl(matches, url));
-        }*/
-
-        /*RouteManager.addRoute("/test", client -> {
-            System.out.println(client.request().headers().get(HttpHeaderNames.HOST));
-            client.send("Hello, World!");
-        });*/
-
-        RouteManager.addRoute("/test", client -> {
+        RouteManager.addRoute("/", client -> {
             if (client.session().getString("test") == null) {
                 client.session().set("test", "" + new Random().nextInt(1000));
             }
@@ -67,7 +18,7 @@ public class Main {
         Settings.getInstance().setSaveSessions(false);
 
         try {
-            new WebServer(80).start();
+            new WebServer(81).start();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
